4a27359633d59d804184f6a91794ee90edddb233
adrianus123 <adrianussimarmata9@gmail.com>
2023-08-11 23:01:40 +0700

    install css-loader & style-loader

--- Belajar Fundamental Front-End Web Development/CocktailPedia/webpack.common.js
+++ Belajar Fundamental Front-End Web Development/CocktailPedia/webpack.common.js
@@ -7,6 +7,29 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
